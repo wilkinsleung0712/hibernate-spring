77f4ea0b31e62d753a28cff76a9c9a1d19dd1cf6
wilkinsleung0712 <wilkinsweiqiangliang@gmail.com>
2014-12-24 11:52:29 +1100

    create new branch of IBM_VERSION

--- src/model/dao/impl/EmployeeDAOImpl.java
+++ src/model/dao/impl/EmployeeDAOImpl.java
@@ -1,15 +1,24 @@
 package model.dao.impl;
-
 import model.Department;
 import model.Employee;
 import model.dao.EmployeeDao;
 
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class EmployeeDAOImpl implements EmployeeDao{
 
+	//logging function
+	private static final Logger LOG=LoggerFactory.getLogger(EmployeeDAOImpl.class);
+	
+	@Autowired
+	private SessionFactory sessionFactory;
 	@Override
 	public void saveEmployee(Employee emp) {
 		// TODO Auto-generated method stub
-		
+	
 	}
 
 	@Override
